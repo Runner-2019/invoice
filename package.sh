c58bf3186573f0fab997cf3108e9a5df17e6bc1e
xiaomingzhang <xiaomingZhang2020@outlook.com>
2024-08-04 18:52:49 +0800

    add version 0.0.1

--- package.sh
+++ package.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-pyinstaller -D calculate.py --collect-all paddleocr --collect-all pyclipper --collect-all imghdr --collect-all skimage --collect-all imgaug --collect-all scipy.io --collect-all lmdb --collect-all Cython --collect-all requests
+pyinstaller -w calculate.py -i resource/rabbit.png --collect-all paddleocr --collect-all pyclipper --collect-all imghdr --collect-all skimage --collect-all imgaug --collect-all scipy.io --collect-all lmdb --collect-all Cython --collect-all requests
 
-cp /opt/homebrew/Cellar/openssl@3/3.3.1/lib/libcrypto.3.dylib dist/main/_internal
+cp /opt/homebrew/Cellar/openssl@3/3.3.1/lib/libcrypto.3.dylib dist/calculate/_internal
+
+zip -r 计算发票.zip dist/calculate
 
 
